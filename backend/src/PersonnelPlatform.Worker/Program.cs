2c41609defc87b2a2dc87009c60ca647a6fec1a2
oncunur <oncunur@icloud.com>
2026-08-23 08:12:17 +0300

    feat: register document status worker

--- backend/src/PersonnelPlatform.Worker/Program.cs
+++ backend/src/PersonnelPlatform.Worker/Program.cs
@@ -1,9 +1,13 @@
+using PersonnelPlatform.Application.Documents;
 using PersonnelPlatform.Infrastructure;
 using PersonnelPlatform.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.AddScoped<DocumentLifecycleProcessor>();
 builder.Services.AddHostedService<HeartbeatWorker>();
+builder.Services.AddHostedService<DocumentStatusWorker>();
 
 var host = builder.Build();
 await host.RunAsync();
