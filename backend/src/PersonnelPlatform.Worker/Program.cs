4c87d9541749f4d676717fe5ec602b53baf25da1
oncunur <oncunur@icloud.com>
2026-08-23 19:30:02 +0300

    feat(reporting): wire report export worker

--- backend/src/PersonnelPlatform.Worker/Program.cs
+++ backend/src/PersonnelPlatform.Worker/Program.cs
@@ -1,6 +1,7 @@
 using PersonnelPlatform.Application.Administration;
 using PersonnelPlatform.Application.Documents;
 using PersonnelPlatform.Application.Notification;
+using PersonnelPlatform.Application.Reporting;
 using PersonnelPlatform.Application.Workflow;
 using PersonnelPlatform.Infrastructure;
 using PersonnelPlatform.Worker;
@@ -12,11 +13,13 @@ builder.Services.AddScoped<DocumentLifecycleProcessor>();
 builder.Services.AddScoped<AdministrativeReminderProcessor>();
 builder.Services.AddScoped<WorkflowSlaProcessor>();
 builder.Services.AddScoped<NotificationProcessor>();
+builder.Services.AddScoped<ReportExportProcessor>();
 builder.Services.AddHostedService<HeartbeatWorker>();
 builder.Services.AddHostedService<DocumentStatusWorker>();
 builder.Services.AddHostedService<AdministrativeReminderWorker>();
 builder.Services.AddHostedService<WorkflowSlaWorker>();
 builder.Services.AddHostedService<NotificationCenterWorker>();
+builder.Services.AddHostedService<ReportExportWorker>();
 
 var host = builder.Build();
 await host.RunAsync();
